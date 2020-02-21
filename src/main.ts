7a2e3b1a15d7def461a4e90375820f5e66481d73
ken <ken@ken.com>
2020-02-21 20:59:11 +0800

    Revert "init"
    This reverts commit 11735c01e351c32ff03c3578f9bfeac06a8c6889.

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,16 @@
-/*!
+import './polyfills';
 
-=========================================================
-* Material Dashboard Angular - v2.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-angular2
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-angular2/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-import 'hammerjs';
 
-if (environment.production) {
-  enableProdMode();
-}
+platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
+  // Ensure Angular destroys itself on hot reloads.
+  if (window['ngRef']) {
+    window['ngRef'].destroy();
+  }
+  window['ngRef'] = ref;
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+  // Otherwise, log the boot error
+}).catch(err => console.error(err));
